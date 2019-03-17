ae18293b1f80d6a582f69844bbcf0d069dfaa1ec
HelioStrike <rageofepicfury@gmail.com>
2019-03-17 10:19:07 +0530

    styled the distributors list a little

--- views/DistributorScreen.js
+++ views/DistributorScreen.js
@@ -1,32 +1,48 @@
 import React from 'react';
-import {Alert, Platform, StyleSheet, Text, View, ScrollView, Image, FlatList } from 'react-native';
-import Bananas from './utils/Bananas';
+import {Alert, Platform, StyleSheet, Text, View, ScrollView, Image, FlatList, RefreshControl } from 'react-native';
 import ErrorMessage from './utils/ErrorMessage';
 import DistributorListItem from './utils/DistributorListItem';
 import firebase from 'firebase';
 
 export default class DistributorScreen extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {distributors: [], errorMessage: ''};
-        this.distributors = [];
-        firebase.database().ref('distributors').once('value')
-        .then(distributors => {
-            distributors = JSON.parse(JSON.stringify(distributors));
-            Object.keys(distributors).map((id) => this.distributors.push({ username: distributors[id].username, latitude: distributors[id].latitude
-                , longitude: distributors[id].longitude, foodUnits: distributors[id].foodUnits}));
-            this.setState({ distributors: this.distributors });
-        })
-        .catch(error => this.setState({ errorMessage: error.message }));
+  constructor(props) {
+    super(props);
+    this.state = {distributors: [], errorMessage: '', refreshing: false};
+    this.distributors = [];
+    this.fetchData();
+  }
 
-    }
+  fetchData() {
+    return (firebase.database().ref('distributors').once('value')
+    .then(distributors => {
+        distributors = JSON.parse(JSON.stringify(distributors));
+        Object.keys(distributors).map((id) => this.distributors.push({ username: distributors[id].username, latitude: distributors[id].latitude
+            , longitude: distributors[id].longitude, foodUnits: distributors[id].foodUnits}));
+        this.setState({ distributors: this.distributors });
+    })
+    .catch(error => this.setState({ errorMessage: error.message })));
+  }
+
+  _onRefresh = () => {
+    this.setState({refreshing: true});
+    this.fetchData().then(() => {
+      this.setState({refreshing: false});
+    });
+  }
 
   render() {
     const {navigate} = this.props.navigation;
     const cuser = JSON.parse(JSON.stringify(this.props.navigation.getParam('user')));
     return (
-      <ScrollView style={styles.container}>
+      <ScrollView style={styles.container}
+        refreshControl={
+          <RefreshControl
+            refreshing={this.state.refreshing}
+            onRefresh={this._onRefresh}
+          />
+        }
+      >
 
         <FlatList
             data={this.state.distributors}
@@ -34,7 +50,6 @@ export default class DistributorScreen extends React.Component {
              foodUnits={item.foodUnits}></DistributorListItem>}
         />
 
-        <Bananas></Bananas>
         <ErrorMessage errorMessage={this.state.errorMessage} />
         
       </ScrollView>
--- views/utils/DistributorListItem.js
+++ views/utils/DistributorListItem.js
@@ -3,35 +3,40 @@ import {Alert, Platform, StyleSheet, Text, View, ScrollView, Image, FlatList, Bu
 
 export default class DistributorListItem extends React.Component {
 
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+      super(props);
+  }
 
-    openMap(latitude, longitude) {
-        const scheme = Platform.select({ ios: 'maps:0,0?q=', android: 'geo:0,0?q=' });
-        const latLng = `${latitude},${longitude}`;
-        const label = 'Custom Label';
-        const url = Platform.select({
-          ios: `${scheme}${label}@${latLng}`,
-          android: `${scheme}${latLng}(${label})`
-        });
-    
-        Linking.openURL(url); 
-    }
+  openMap(latitude, longitude) {
+      const scheme = Platform.select({ ios: 'maps:0,0?q=', android: 'geo:0,0?q=' });
+      const latLng = `${latitude},${longitude}`;
+      const label = 'Custom Label';
+      const url = Platform.select({
+        ios: `${scheme}${label}@${latLng}`,
+        android: `${scheme}${latLng}(${label})`
+      });
+  
+      Linking.openURL(url); 
+  }
 
   render() {
    return (
       <View style={styles.container}>
-        <Text>{this.props.username}</Text>
-
-        <View style={{alignSelf: 'flex-end'}}>
-            <Button title="View Map" onPress={() => this.openMap(this.props.latitude, this.props.longitude)}></Button>
+        <View style={styles.cardLeftContainer}>
+          <Text style={{color: '#4CBB17', fontSize: 24}}>{this.props.username}</Text>
+          <Text>
+            <Text style={{color: '#DC143C', fontSize: 20}}>Units left:</Text> {this.props.foodUnits}
+          </Text>
         </View>
         
-        <Text>Units left: {this.props.foodUnits}</Text>
-        
-        <View style={{alignSelf: 'flex-end'}}>
-            <Button title="Claim Unit"></Button>
+        <View style={styles.cardRightContainer}>
+          <View style={styles.cardBtnContainer}>
+              <Button title="View Map" onPress={() => this.openMap(this.props.latitude, this.props.longitude)}></Button>
+          </View>
+
+          <View style={styles.cardBtnContainer}>
+              <Button title="Claim Unit"></Button>
+          </View>
         </View>
       </View>
     );
@@ -42,13 +47,25 @@ export default class DistributorListItem extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    margin: 5,
-    padding: 5,
+    flexDirection: 'row',
+    margin: 10,
+    padding: 10,
     backgroundColor: '#FFFFFF',
-    borderRadius: 6,
+    borderBottomWidth: 3,
   },
   btnContainer: {
     margin: 20, 
     width: 200,
   },
+  cardLeftContainer: {
+    width: '50%',
+  },
+  cardRightContainer: {
+    width: '50%',
+  },
+  cardBtnContainer: {
+    margin: 5,
+    width: 100,
+    alignSelf: 'flex-end'
+  }
 });
