48300e9e2abaa07f09a950aae12b2c8d0741c1d8
HelioStrike <rageofepicfury@gmail.com>
2019-03-19 00:37:20 +0530

    added checks in SignupScreen

--- views/auth/SignupScreen.js
+++ views/auth/SignupScreen.js
@@ -8,7 +8,7 @@ export default class SignupScreen extends React.Component {
   constructor(props) {
     super(props);
     this._onSignup = this._onSignup.bind(this);
-    this.state = {email: "", username: "", password: "", phoneno: "", errorMessage: "", disabled: false};
+    this.state = {email: "", username: "", password: "", retypePassword: "", phoneno: "", errorMessage: "", disabled: false};
     /*
     firebase.database().ref('distributors').push({ email: 'distro2@gmail.com', username: 'distro2',
      latitude: 30.3850, longitude: 98.4867, phoneno: 987654321, foodUnits: 40, claimants: ['sandeep', 'sharat'], })
@@ -18,16 +18,20 @@ export default class SignupScreen extends React.Component {
 }
 
   handleSignUp = () => {
-    this.setState({disabled: true});
-    firebase
-      .auth()
-      .createUserWithEmailAndPassword(this.state.email, this.state.password)
-      .then(() => { 
-        firebase.database().ref('users').push({ email: this.state.email, username: this.state.username, phoneno: this.state.phoneno })
-        .then(() => this.props.navigation.navigate('Login'))
-        .catch(error => this.setState({ errorMessage: error.message, disabled: false }));
-      })
-      .catch(error => this.setState({ errorMessage: error.message, disabled: false }));
+    if(this.state.username === '' || this.state.phoneno === '' || this.state.password != this.state.retypePassword) {
+      this.setState({errorMessage: "Please enter all the details correctly!"});
+    } else {
+      this.setState({disabled: true});
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(this.state.email, this.state.password)
+        .then(() => { 
+          firebase.database().ref('users').push({ email: this.state.email, username: this.state.username, phoneno: this.state.phoneno })
+          .then(() => this.props.navigation.navigate('Login'))
+          .catch(error => this.setState({ errorMessage: error.message, disabled: false }));
+        })
+        .catch(error => this.setState({ errorMessage: error.message, disabled: false }));  
+    }
   }
 
   _onSignup() {
@@ -67,6 +71,15 @@ export default class SignupScreen extends React.Component {
             placeholder='Password'
             textContentType='password'
             onChangeText={(passwordd) => this.setState({password: passwordd})}
+          />
+        </View>
+
+        <View style={styles.inputContainer}>
+          <TextInput
+            secureTextEntry={true}
+            placeholder='Retype Password'
+            textContentType='password'
+            onChangeText={(retypePasswordd) => this.setState({retypePassword: retypePasswordd})}
           />
         </View>
 
